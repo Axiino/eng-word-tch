cbd7e83c186e39f18db9317ca67af577939509fd
Rymar <m.rymar@cometrica.ru>
2021-05-24 15:52:54 +0300

    first commit

---
+++ src/test/java/com/englishwordteach/engwordtch/EngWordTchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.englishwordteach.engwordtch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngWordTchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
